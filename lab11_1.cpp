5d3c512f314de624f3477aa45217e98d3f43bf1e
Chaichan <chaichanchaisak2549@gmail.com>
2024-12-20 16:26:23 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Press Enter 3 times to reveal your future.\n";
+    
+    for(int i = 1 ; i <= 3 ; i++ )
+    {
+        cin.get();
+    }
+    
+    string grade[] = {"A" , "B+" , "B" , "C+" , "C" , "D+" , "D" , "F" , "W"}; 
+    
+    int ran;
+    srand(time(0));
+    ran = rand() % 9;
+    
+    cout << "You will get " << grade[ran] << " in this 261102.";
+    
+    return 0;
+}
